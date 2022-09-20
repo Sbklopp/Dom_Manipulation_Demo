acb88002752e4bb000a77a83b8e8ea3830a6bcf6
Sbklopp <sbklopp11@gmail.com>
2022-09-20 17:08:29 -0400

    added reset values function and 3rd output that automatically updates

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const demo_input = document.getElementById('demo_input_container')
 const radio_button = document.getElementById('radio_button_container')
 const output_a_hide = document.getElementById('output_A_container')
 const output_b_hide = document.getElementById('output_B_container')
+const output_c_hide = document.getElementById('output_C_container')
 
 
 /////////////////////////////////////////////////////////////
 const button_container = document.getElementById('button_container')
 const button_hide = document.getElementById('demo_button_hide')
+const button_clear = document.getElementById('demo_button_clear')
 
 ///////////////////////////////////////////////////////////////
 const option1 = document.getElementById('option_1')
@@ -19,7 +21,7 @@ const option2 = document.getElementById('option_2')
 //////////////////////////////////////////
 //input variables
 //////////////////////////////////////
-const number = document.getElementById('demo_input')
+const input_number = document.getElementById('demo_input')
 
 
 /////////////////////////////////////////////////////
@@ -27,6 +29,7 @@ const number = document.getElementById('demo_input')
 ////////////////////////////////////////////////////
 const output_a = document.getElementById('output_A')
 const output_b = document.getElementById('output_B')
+const output_c = document.getElementById('output_C')
 
 
 
@@ -34,7 +37,8 @@ function hideFields() {
     demo_input.style.display = 'none'
     radio_button.style.display = 'none'
     output_a_hide.style.display = 'none'
-    output_b_hide.style.display = 'none' 
+    output_b_hide.style.display = 'none'
+    output_c_hide.style.display = 'none' 
 }
 
 
@@ -43,7 +47,16 @@ function showFields() {
     radio_button.style.display = 'block'
     output_a_hide.style.display = 'block'
     output_b_hide.style.display = 'block' 
+    output_c_hide.style.display = 'block'
 }
+
+function clearFields(){
+    input_number.value = ''
+    output_a.value = ''
+    output_b.value = ''
+    output_c.value = ''
+}
+
 
 function hide_stuff(){
     button_container.addEventListener('click', (e) => {
@@ -61,30 +74,45 @@ function show_stuff(){
     }
 )}
 
+
+function reset_values(){
+    button_container.addEventListener('click', (e) => {
+        if (e.target.id === 'demo_button_clear') {
+            clearFields()
+        }
+
+    })
+}
+
 //////////////////////////////////////////////////////
 
 
 
 hide_stuff()
 show_stuff()
-// hide_stuff()
+reset_values()
 
 
 function doMath(){
     radio_button.addEventListener('click', (e) => {
         if (e.target.id === 'option_1') {
             // number.addEventListener('input')
-            output_a.value = number.value
+            output_a.value = input_number.value
         }
         else if (e.target.id === 'option_2') {
-            output_b.value = number.value * 2
+            output_b.value = input_number.value * 2
         }
+    })
+}
 
-
-
-
+function total(){
+    radio_button.addEventListener('click', (e) => {
+        output_c.value = Number(output_a.value) + Number(output_b.value)  
     })
 }
 
 
+
+
 doMath()
+total()
